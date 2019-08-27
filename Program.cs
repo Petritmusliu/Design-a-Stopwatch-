7c2c4df8f1557c4dc8a1cfa821622b42da1ebe2c
Petritmusliu <50852385+Petritmusliu@users.noreply.github.com>
2019-08-27 22:38:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch=new Stopwatch();
+            var state = false;
+
+            while (true)
+            {
+                if (!state)
+                    Console.WriteLine("Type s to Start,\nx to Stop\nr to Repeat\ne to Exit\n");
+
+                var input = Console.ReadLine();
+
+                if (input.ToLower().Equals("s"))
+                {
+                    stopWatch.Start();
+                    Console.WriteLine("StopWatch has started'\nx to Stop\nr to Repet\nany other key to Exit\n");
+                    state = true;
+                }
+
+                if(input.ToLower().Equals("x"))
+                {
+                    stopWatch.Stop();
+                    Console.WriteLine("Time Span was-> "+stopWatch.GetInterval());
+                    state = false;
+                }
+
+                if (input.ToLower().Equals("r"))
+                {
+                    state = false;
+                    Console.WriteLine("StopWatch has been Restarted");
+                 continue;   
+                }
+
+                if (!input.ToLower().Equals("s")&&!input.ToLower().Equals("x")&&!input.ToLower().Equals("r"))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running ;
+
+        public void Start()
+        {
+            if (_running)
+                throw new InvalidOperationException("StopWatch is already running.");
+
+            _running = true;
+            _startTime = DateTime.Now;
+            
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("StopWatch is not running.");
+            _running = false;
+            _endTime = DateTime.Now;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime -_startTime;
+        }
+    }
+}
